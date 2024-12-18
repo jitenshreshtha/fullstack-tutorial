50c3cda053b8616342ea020aeaa7c90acf8253d6
Jiten Shreshtha <jshreshtha0448@conestogac.on.ca>
2024-12-18 11:32:48 -0500

    template engine installed and routing done

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require("path");
 require('dotenv').config();
+const ejs = require('ejs');
 
 const app = express();
 
 // global middlewares
 app.use(express.static('public'));
+app.set('view engine','ejs');
 
 // global environment variables
 const PORT = process.env.PORT;
@@ -13,16 +15,16 @@ const PORT = process.env.PORT;
 
 // routes
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "pages/index.html"));
+    res.render('index');
 })
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'pages/about.html'));
+   res.render('about');
 })
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render('contact');
 })
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "pages/post.html"));
+    res.render('post');
 })
 
 app.listen(PORT, () => {
